048e23e29132844afed5f8bd54aef0010c7702e4
Barbara Vreede <b.vreede@gmail.com>
2016-06-13 20:06:19 +0100

    excluded same discrete category

--- plotviz.py
+++ plotviz.py
@@ -8,7 +8,7 @@ sns.set_style("ticks")
 sns.set_context("poster")
 #sns.despine(trim=True)
 
-df = pd.read_csv("dataset_random.csv")
+df = pd.read_csv("collect_data.csv")
 
 #colors = np.where(df.simultaneous_fixation == True, 'r', 'g')
 #plt.scatter(df.slot_duration, df.pvalue, c=colors)
@@ -19,28 +19,61 @@ df = pd.read_csv("dataset_random.csv")
 #plt.show()
 #plt.figure()
 
-def makeplot(title,filename,xlabel,xdata,ylabel,ydata,catlabel,catdata):
-	ax = sns.violinplot(x=xdata, y=ydata, hue=catdata, data=df, palette="Set1", scale="count",inner="quartile", bw=.1)
+
+discrete_cats = ["genetics_bool","slot_duration","simultaneous_fixation","clutch_m","clutch_sd","noise_sd"]
+output_cats = ["chisquare","pvalue","n_embryos","smallest_n","total_clutches"]
+
+labels = {"genetics_bool":"Clutch genetics","slot_duration":"Fixation frequency","simultaneous_fixation":"Simultaneous fixation","clutch_m":"Mean clutch size","clutch_sd":"Clutch size standard deviation","noise_sd":"Noise standard deviation","chisquare":"Chi square","pvalue":"p-value","n_embryos":"total numer of embryos","smallest_n":"n embryos per age category","total_clutches":"total number of clutches"}
+
+orders = {"genetics_bool":[True,False],"slot_duration":[15,30,60,120],"simultaneous_fixation":[True,False],"clutch_m":[4,10,20],"clutch_sd":[1,6,15],"noise_sd":[5,20]}
+
+
+def makeplot(title,filename,xlabel,xdata,xorder,ylabel,ydata,catlabel,catdata):
+	ax = sns.violinplot(x=xdata, y=ydata, hue=catdata, data=df, palette="Set1", scale="count",inner="quartile", order=xorder, bw=.1)
 	ax.set_title("")
 	ax.set_xlabel(xlabel)
 	ax.set_ylabel(ylabel)
 	plt.savefig("/home/barbara/Dropbox/oncopeltus/susan-lisa/%s.png" %filename)
+	plt.clf()
 	plt.close()
 
-## Fill up the plot
-title = ""
-filename = ""
 
-## X axis ##
-xlabel = "Fixation frequency (in minutes)"
-xdata =  "slot_duration"
+##Discrete categories in the dataset
+#"genetics_bool"
+#"slot_duration"
+#"simultaneous_fixation"
+#"clutch_m"
+#"clutch_sd"
+#"noise_sd"
 
-## Y axis ##
-ylabel = "p-value" 
-ydata = "pvalue" #pvalue
+## Output/continuous categories in the dataset
+#"chisquare"
+#"pvalue"
+#"n_embryos"
+#"smallest_n"
+#"total_clutches"
 
-## Categories ##
-catlabel = ""
-catdata = "simultaneous_fixation"
+for cc in output_cats:
+	for dc1 in discrete_cats:
+		for dc2 in discrete_cats:
+			if dc1 == dc2:
+				continue
+			## Fill up the plot
+			title = "%s by %s per %s" %(labels[cc],labels[dc1],labels[dc2])
+			filename = "%s-%s-%s" %(cc,dc1,dc2)
+	
+			## X axis ##
+			xlabel = labels[dc1]
+			xdata =  dc1
+			xorder = orders[dc1]
 
-makeplot()
+			## Y axis ##
+			ylabel = labels[cc] 
+			ydata = cc
+
+			## Categories ##
+			catlabel = labels[dc2]
+			catdata = dc2
+
+			makeplot(title,filename,xlabel,xdata,xorder,ylabel,ydata,catlabel,catdata)
+
