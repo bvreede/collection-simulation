8da5461975f1cbaa60708687edc145c83eb60bfa
Barbara Vreede <b.vreede@gmail.com>
2016-06-13 22:39:41 +0100

    added statistics and limited dataset

--- plotviz.py
+++ plotviz.py
@@ -9,6 +9,14 @@ sns.set_context("poster")
 #sns.despine(trim=True)
 
 df = pd.read_csv("collect_data.csv")
+#data=df.loc[(df["slot_duration"]==15) & (df["genetics_bool"]==True)]
+#data2 = data.loc[data["pvalue"] < 0.05]
+
+data_adjust = df.loc[(df["smallest_n"]>30) & (df["n_embryos"]>200)]
+
+#print "15 min AND YES genetics:", len(data)
+#print "Of those, pval <0.05:", len(data2)
+#print "Percentage:", len(data2)/float(len(data))*100
 
 #colors = np.where(df.simultaneous_fixation == True, 'r', 'g')
 #plt.scatter(df.slot_duration, df.pvalue, c=colors)
@@ -29,11 +37,11 @@ orders = {"genetics_bool":[True,False],"slot_duration":[15,30,60,120],"simultane
 
 
 def makeplot(title,filename,xlabel,xdata,xorder,ylabel,ydata,catlabel,catdata):
-	ax = sns.violinplot(x=xdata, y=ydata, hue=catdata, data=df, palette="Set1", scale="count",inner="quartile", order=xorder, bw=.1)
+	ax = sns.violinplot(x=xdata, y=ydata, hue=catdata, data=data_adjust, palette="Set1", scale="count",inner="quartile", order=xorder, bw=.1)
 	ax.set_title("")
 	ax.set_xlabel(xlabel)
 	ax.set_ylabel(ylabel)
-	plt.savefig("/home/barbara/Dropbox/oncopeltus/susan-lisa/%s.png" %filename)
+	plt.savefig("/home/barbara/Dropbox/oncopeltus/susan-lisa/adjusteddata-%s.png" %filename)
 	plt.clf()
 	plt.close()
 
@@ -76,4 +84,6 @@ for cc in output_cats:
 			catdata = dc2
 
 			makeplot(title,filename,xlabel,xdata,xorder,ylabel,ydata,catlabel,catdata)
+
+
 
